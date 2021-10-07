e6973e22d8010e99496b46aadc707cb04826af1b
andrefmoreira <82107406+andrefmoreira@users.noreply.github.com>
2021-10-07 11:41:02 +0100

    Update Resolução.py

--- Resolução.py
+++ Resolução.py
@@ -1,11 +1,32 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
-P = [1, 2, 3, 4, 5, 6, 2, 3, 5, 2, 2, 4, 6]
+alfabeto = {}
 
-ola = "123"
-def histograma(data):
-    plt.hist(data)
-    plt.show()
+def cria_alfabeto(alfabeto):
+    for i in range(ord('A') , ord('Z')+1):
+        alfabeto[i] = 0
+    for i in range(ord('a') , ord('z')+1):
+        alfabeto[i] = 0
 
 
-histograma(P)
+
+def ocorrencias(alfabeto , string):
+    for i in range(len(string)):
+        for k in range(ord('A') , ord('Z')+1):
+            if(string[i] == chr(k)):
+                alfabeto[k] += 1
+        for j in range(ord('a') , ord('z')+1):
+            if(string[i] == chr(j)):
+                alfabeto[j] += 1
+
+            
+                
+
+cria_alfabeto(alfabeto)
+string = "12331sdasdasd"
+ocorrencias(alfabeto,string)
+max_value = max(alfabeto.values())
+print(alfabeto)
+plt.ylim([0,max_value])
+plt.plot( list(alfabeto) , list(alfabeto.values())  , "r") 
