0e66eaf0e0d0b03af26c5c2644828b526bc91f15
PolpEdu <69104634+PolpEdu@users.noreply.github.com>
2021-10-07 12:52:28 +0100

    Update Resolução.py

--- Resolução.py
+++ Resolução.py
@@ -1,32 +1,62 @@
 import numpy as np
+import random
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from scipy.io import wavfile
+import math
 
-alfabeto = {}
-
-def cria_alfabeto(alfabeto):
-    for i in range(ord('A') , ord('Z')+1):
-        alfabeto[i] = 0
-    for i in range(ord('a') , ord('z')+1):
-        alfabeto[i] = 0
+data = np.random.randint(0, 10, size=20)
 
 
+def criarhist(data, alfabeto):
+    print("fjlsnka")
+    # ocorrencias = [i.count for x in data for i in alfabeto]
+    # print(ocorrencias)
 
-def ocorrencias(alfabeto , string):
-    for i in range(len(string)):
-        for k in range(ord('A') , ord('Z')+1):
-            if(string[i] == chr(k)):
-                alfabeto[k] += 1
-        for j in range(ord('a') , ord('z')+1):
-            if(string[i] == chr(j)):
-                alfabeto[j] += 1
+    # for x in data:
+    #    plt.xtics()
+    #    plt.bar()
+    # plt.show()
 
-            
-                
 
-cria_alfabeto(alfabeto)
-string = "12331sdasdasd"
-ocorrencias(alfabeto,string)
-max_value = max(alfabeto.values())
-print(alfabeto)
-plt.ylim([0,max_value])
-plt.plot( list(alfabeto) , list(alfabeto.values())  , "r") 
+def lerficheiro(nome):
+    ext = nome.split(".")[1]
+    PATH = "./data/" + nome
+
+    if ext == "txt":
+        # Caracteres ASCII, existe 127 caracteres ASCII
+        alfabeto = [x for x in range(0, 127 + 1)]
+        with open(PATH, "r", encoding='ASCII') as f:
+            data = f.read()
+
+    elif ext == "bmp":
+        # bmp os valores usados (preto a branco) vão de 0 a 255
+        alfabeto = [x for x in range(0, 255 + 1)]
+        datasquare = mpimg.imread(PATH) 
+        data = datasquare.flatten()
+
+    elif ext == "wav":
+
+        [fs, info] = wavfile.read(PATH)
+
+        # como o dtype é sempre int*BITS*
+        # vou e tiro o numero de bits por cada amostra para conseguir o maior valor
+        print(str(info.dtype).split("int")[1])
+        max = math.pow(2, int(str(info.dtype).split("int")[1]))
+
+        # Wav normalizado vai de 0 a max+1.
+        alfabeto = [x for x in range(0, int(max + 1))]
+
+        data = [fs, info]
+
+    print(f"Alfabeto:{alfabeto}\n"
+          f"Data:{data}")
+    return alfabeto, data
+
+
+# def entropia(data):
+
+
+if __name__ == "__main__":
+    [alfabeto, data] = lerficheiro("english.txt")
+    criarhist(data, alfabeto)
